b2c7f3369ac4c2b515cc3a2ec6dc9096e4da71da
Eber Lugarezi <eberqueiroz333@gmail.com>
2024-06-26 12:28:01 -0300

    aula 2

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ print ("# Escolha a opçao desejada:            #")
 print ("#                                      #")
 print ("# 1. Fibonacci                         #")
 print ("########################################")
+
+try:
+    choice = int (input())
+
+    if choice == 1:
+        print ("FIB")
+except: ValueError as error:
